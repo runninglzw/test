f2f9560d9fcf8bace7ae246d4864fa71f135ced8
runninglzw <459437440@qq.com>
2015-03-20 17:33:03 +0800

    简单实现Tcp通信
    客户端与服务端的通信

---
+++ tcp通信/tcp通信客户端（tcpclient类）/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //发送文字
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TcpClient tc = new TcpClient();
+            //使用指定ip地址和端口号连接服务器
+            tc.Connect(IPAddress.Parse(textBox1.Text), Int32.Parse(textBox2.Text));
+            MessageBox.Show("请求已发送！");
+            //获得连接到服务器的数据流
+            NetworkStream ns = tc.GetStream();
+            //选择要发送给服务器的内容转换为字节流
+            byte[] sendby = Encoding.UTF8.GetBytes(richTextBox1.Text);
+            //把字节流写入数据流，发送
+            ns.Write(sendby,0,sendby.Length);
+            ns.Close();
+            tc.Close();
+        }
+        //发送文件内容
+        private void button2_Click(object sender, EventArgs e)
+        {
+            TcpClient tc = new TcpClient();
+            tc.Connect(IPAddress.Parse(textBox1.Text), Int32.Parse(textBox2.Text));
+            NetworkStream ns = tc.GetStream();
+            //打开指定路径上的文件，存入Filestream流中
+            FileStream fs = File.Open("F:/c#/c#编程/c#选择排序.txt",FileMode.Open);
+            //读取一个字节，指针上移一个字节
+            int data = fs.ReadByte();
+            while (data != -1)
+            {
+                ns.WriteByte((byte)data);
+                data = fs.ReadByte();
+            }
+            fs.Close();
+            ns.Close();
+            tc.Close();
+        }
+    }
+}
---
+++ tcp通信/tcp通信服务端（tcplistener类）/WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            //定义后台线程执行监听方法
+            Thread t = new Thread(new ThreadStart(listen));
+            //启动线程
+            t.Start();
+            richTextBox1.Text = "正在监听。。。";
+
+        }
+        protected delegate void updatedisplay(string text);
+        private void listen()
+        {
+                IPAddress ipaddress = IPAddress.Parse("127.0.0.1");
+                int port = 8080;
+                //侦听传入的连接请求
+                TcpListener tl = new TcpListener(ipaddress, port);
+
+                tl.Start();
+               // MessageBox.Show("检测到客户端请求");
+                while (true)//一直获取客户端的消息
+                {
+                    //从队列中获取连接
+                    TcpClient tc = tl.AcceptTcpClient();
+                    MessageBox.Show("检测到客户端请求");
+                    //获取连接的数据流
+                    NetworkStream ns = tc.GetStream();
+                    //读取数据流
+                    StreamReader sr = new StreamReader(ns);
+                    string result = sr.ReadToEnd();
+                    //不能从后台线程直接访问界面，用invoke方法告诉窗体线程完成相应操作，这里使用委托，把得到的result字符串作为object数组传递给update方法的text参数
+                    Invoke(new updatedisplay(update), new object[] { result });
+                    
+                    //update(result);
+                    tc.Close();
+                    ns.Close();
+                }
+        }
+
+        private void update(string text)
+        {
+            richTextBox1.Text = text;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //listen();
+        }
+    }
+}
