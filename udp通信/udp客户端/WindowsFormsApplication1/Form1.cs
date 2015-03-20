b178c28ef38e9975d4ef2269e3355d8fa37cc472
runninglzw <459437440@qq.com>
2015-03-20 18:00:31 +0800

    udp的通信
    客户端和服务端使用udp协议通信

---
+++ udp通信/udp客户端/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            UdpClient uc = new UdpClient();
+            string send = textBox1.Text;
+            MessageBox.Show(send);
+            byte[] sendbyte = Encoding.UTF8.GetBytes(send);
+            IPEndPoint ipendpoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8080);
+            //uc.Connect(ipendpoint);
+            uc.Send(sendbyte, sendbyte.Length, ipendpoint);
+        }
+    }
+}
---
+++ udp通信/udp服务端/WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Net.Sockets;
+using System.Net;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Thread th = new Thread(new ThreadStart(listen));
+            th.Start();
+            richTextBox1.Text = "监听中。。。";
+        }
+        protected delegate void updatedisplay(string s);
+        private void listen()
+        {
+                UdpClient uc = new UdpClient();
+                IPEndPoint ip = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8080);
+                uc.Connect(ip);
+                byte[] receive = uc.Receive(ref ip);
+                string result = Encoding.UTF8.GetString(receive);
+                Invoke(new updatedisplay(update), new object[]{result});
+        }
+
+        private void update(string s)
+        {
+            richTextBox1.Text = s;
+        }
+    }
+}
