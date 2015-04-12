1be404c7ea2029caaad403a0f4769ea2e375caff
runninglzw <459437440@qq.com>
2015-04-12 20:10:18 +0800

    TCP、UDP、XML的解析

---
+++ XML的解析/WebApplication1/JavaScript1.js
@@ -0,0 +1,50 @@
+﻿alert("开始解析！");
+loadXML = function (xmlFile) {
+    alert("loadxml已启动！");
+    var xmlDoc = null;
+    //判断浏览器的类型
+    //支持IE浏览器
+    if (!window.DOMParser && window.ActiveXObject) {
+        var xmlDomVersions = ['MSXML.2.DOMDocument.6.0', 'MSXML.2.DOMDocument.3.0', 'Microsoft.XMLDOM'];
+        for (var i = 0; i < xmlDomVersions.length; i++) {
+            try {
+                xmlDoc = new ActiveXObject(xmlDomVersions[i]);
+                break;
+            } catch (e) {
+            }
+        }
+    }
+        //支持Mozilla浏览器
+    else if (document.implementation && document.implementation.createDocument) {
+        try {
+            /* document.implementation.createDocument('','',null); 方法的三个参数说明
+             * 第一个参数是包含文档所使用的命名空间URI的字符串； 
+             * 第二个参数是包含文档根元素名称的字符串； 
+             * 第三个参数是要创建的文档类型（也称为doctype）
+             */
+            xmlDoc = document.implementation.createDocument('', '', null);
+        } catch (e) {
+        }
+    }
+    else {
+        return null;
+    }
+
+    if (xmlDoc != null) {
+        xmlDoc.async = false;
+        xmlDoc.load(xmlFile);
+    }
+    return xmlDoc;
+}
+    function ajax() {
+        alert("方法调用成功！")
+        var xmldoc = loadXML(text.xml)
+        alert("获得xml文件");
+        var elements = xmlDoc.getElementsByTagName("Company");
+        alert(elements);
+        for (var i = 0; i < elements.length; i++) {
+            var name = elements[i].getElementsByTagName("cNname")[0].firstChild.nodeValue;
+            var ip = elements[i].getElementsByTagName("cIP")[0].firstChild.nodeValue;
+
+        }
+    }
---
+++ XML的解析/WebApplication1/WebForm1.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml;
+
+namespace WebApplication1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            XmlDocument xmldoc = new XmlDocument();
+            //加载xml文档
+            xmldoc.Load("F:\\ASP.NET\\WebApplication1\\WebApplication1\\text.xml");
+            //获取xml文档的根节点
+            var root = xmldoc.DocumentElement;
+            //获取匹配表达式的第一个结点
+            XmlNode no = xmldoc.SelectSingleNode("PlaceSearchResponse/results/result/name");
+            Response.Write(no.InnerText);
+            //获取匹配表达式的节点列表
+            XmlNodeList nodes = xmldoc.SelectNodes("PlaceSearchResponse/results/result/name");
+            foreach (XmlNode node in nodes)
+            {
+                //Response.Write(node.Attributes["name"].Value);
+                //获取该结点的内容
+                Response.Write("</br>"+node.InnerText);
+            }
+        }
+
+
+    }
+}
