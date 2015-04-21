d5d7fe315b9626d6dfaef3258a6c8031c2fc4ca0
runninglzw <459437440@qq.com>
2015-04-21 16:08:50 +0800

    自制闹钟
    使用C#，Win Form实现的一个简易闹钟

---
+++ 自制计时器/自制计时器/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace 自制计时器
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(146, 163);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(60, 24);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "确定";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(212, 163);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(60, 24);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "取消";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(3, 139);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(42, 21);
+            this.numericUpDown1.TabIndex = 2;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(51, 139);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(42, 21);
+            this.numericUpDown2.TabIndex = 3;
+            this.numericUpDown2.ValueChanged += new System.EventHandler(this.numericUpDown2_ValueChanged);
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(99, 139);
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(42, 21);
+            this.numericUpDown3.TabIndex = 4;
+            this.numericUpDown3.ValueChanged += new System.EventHandler(this.numericUpDown3_ValueChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(3, 50);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(137, 21);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(3, 166);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(137, 21);
+            this.textBox2.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(189, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(189, 119);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "label2";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(7, 16);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(67, 21);
+            this.textBox3.TabIndex = 9;
+            this.textBox3.Text = "系统时间：";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(7, 100);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(67, 21);
+            this.textBox4.TabIndex = 10;
+            this.textBox4.Text = "时间定时：";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(175, 16);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(67, 21);
+            this.textBox5.TabIndex = 11;
+            this.textBox5.Text = "定时提示：";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+
+    }
+}
+
---
+++ 自制计时器/自制计时器/Form1.cs
@@ -0,0 +1,324 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+using System.Runtime.InteropServices;
+
+namespace 自制计时器
+{
+    public partial class Form1 : Form
+    {
+        int inth, intm, ints;
+        public Form1()
+        {
+            
+            InitializeComponent();
+            timer1.Enabled = true;//启动form的同时启动timer1组件，即获取系统时间
+            label1.Text = "";//将提示框置空
+            label2.Text = "";
+        }
+        /// <summary>
+        /// 启动timer组件开始倒计时
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DateTime gettime = Convert.ToDateTime(DateTime.Now.ToString());//获取系统当前时间
+            DateTime ontime = Convert.ToDateTime(textBox2.Text.Trim().ToString());//获取指定的时间
+            TimeSpan sta=TimeSpan.FromHours(0);//初始化一个表示时间间隔的对象
+            long dat = DateAndTime.DateDiff("s", gettime, ontime, FirstDayOfWeek.Sunday, FirstWeekOfYear.FirstFourDays);//获取时间差，固定方法
+            if (dat > 0)//还未到达指定的时间
+            {
+                if (timer2.Enabled != true)//timer组件还未启动，即还未开始计时
+                {
+                    timer1.Enabled = true;//获取系统时间的组件开始启动
+                    timer2.Enabled = true;//获取时间差的组件开始启动
+                    label2.Text = "闹钟已启动！";
+                    label1.Text = "剩余" + dat.ToString() + "秒";
+                }
+                else
+                {
+                    MessageBox.Show("闹钟已启动，请取消后再启动！");//避免重复启动
+                }
+            }
+            else//指定时间已经过去时
+            {
+                long hour = 24 * 3600 + dat;//将指定时间设为下一天
+                timer1.Enabled = true;
+                timer2.Enabled = true;
+                label2.Text = "闹钟已启动！";
+                label1.Text = "剩余" + hour.ToString() + "秒";//同理上
+            }
+        }
+        /// <summary>
+        /// 获取时间差的组件的实现
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            DateTime gettime = Convert.ToDateTime(DateTime.Now.ToString());
+            DateTime ontime = Convert.ToDateTime(textBox2.Text.Trim().ToString());
+            //TimeSpan sta=TimeSpan.FromHours(0);
+            long dat = DateAndTime.DateDiff("s", gettime, ontime, FirstDayOfWeek.Sunday, FirstWeekOfYear.FirstFourDays);
+            if (dat > 0)
+            {
+                label1.Text = "剩余" + dat.ToString() + "秒";
+            }
+            else
+            {
+                long hour = 24 * 3600 + dat;
+                label1.Text = "剩余" + hour.ToString() + "秒";
+            }
+            if (dat == 0)
+            {
+                //Beep(200, 500);
+                label2.Text = "时间到啦！";
+                timer2.Enabled = false;//时间到则终止组件timer2
+            }
+
+        }
+        /// <summary>
+        /// 设置指定时间的小时值
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            string h, m, s;
+            if (numericUpDown1.Value == 24)//满24小时时
+            {
+                numericUpDown1.Value = 0;
+                inth = Convert.ToInt32(numericUpDown1.Value);
+                //以下代码为控制显示的格式
+                if (inth < 10)
+                {
+                    h = "0" + inth.ToString() + ":";
+                }
+                else
+                {
+                    h = inth.ToString() + ":";
+                }
+                if (intm < 10)
+                {
+                    m = "0" + intm.ToString() + ":";
+                }
+                else
+                {
+                    m = intm.ToString() + ":";
+                }
+                if (ints < 10)
+                {
+                    s = "0" + ints.ToString();
+                }
+                else
+                {
+                    s = ints.ToString();
+                }
+                textBox2.Text = h + m + s;
+            }
+            else//不满24小时
+            {
+                inth = Convert.ToInt32(numericUpDown1.Value);
+                if (inth < 10)
+                {
+                    h = "0" + inth.ToString() + ":";
+                }
+                else
+                {
+                    h = inth.ToString() + ":";
+                }
+                if (intm < 10)
+                {
+                    m = "0" + intm.ToString() + ":";
+                }
+                else
+                {
+                    m = intm.ToString() + ":";
+                }
+                if (ints < 10)
+                {
+                    s = "0" + ints.ToString();
+                }
+                else
+                {
+                    s = ints.ToString();
+                }
+                textBox2.Text = h + m + s;
+            }
+            
+        }
+        /// <summary>
+        /// 设置指定时间的分钟值
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+        { 
+            //以下代码同上
+            string h, m, s;
+            if (numericUpDown2.Value == 60)
+            {
+                numericUpDown2.Value = 0;
+                numericUpDown1.Value = Convert.ToInt32(numericUpDown1.Value) + 1;//60分进位1小时
+                inth = Convert.ToInt32(numericUpDown1.Value);
+                intm = Convert.ToInt32(numericUpDown2.Value);
+                if (inth < 10)
+                {
+                    h = "0" + inth.ToString() + ":";
+                }
+                else
+                {
+                    h = inth.ToString() + ":";
+                }
+                if (intm < 10)
+                {
+                    m = "0" + intm.ToString() + ":";
+                }
+                else
+                {
+                    m = intm.ToString() + ":";
+                }
+                if (ints < 10)
+                {
+                    s = "0" + ints.ToString();
+                }
+                else
+                {
+                    s = ints.ToString();
+                }
+                textBox2.Text = h + m + s;
+            }
+            else
+            {
+                intm = Convert.ToInt32(numericUpDown2.Value);
+                if (inth < 10)
+                {
+                    h = "0" + inth.ToString() + ":";
+                }
+                else
+                {
+                    h = inth.ToString() + ":";
+                }
+                if (intm < 10)
+                {
+                    m = "0" + intm.ToString() + ":";
+                }
+                else
+                {
+                    m = intm.ToString() + ":";
+                }
+                if (ints < 10)
+                {
+                    s = "0" + ints.ToString();
+                }
+                else
+                {
+                    s = ints.ToString();
+                }
+                textBox2.Text = h + m + s;
+            }
+
+        }
+        /// <summary>
+        /// 设置指定时间的秒值
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void numericUpDown3_ValueChanged(object sender, EventArgs e)
+        {
+            string h, m, s;
+            if (numericUpDown3.Value == 60)
+            {
+                numericUpDown3.Value = 0;
+                numericUpDown2.Value = Convert.ToInt32(numericUpDown2.Value) + 1;
+                ints = Convert.ToInt32(numericUpDown3.Value);
+                intm = Convert.ToInt32(numericUpDown2.Value);
+                if (inth < 10)
+                {
+                    h = "0" + inth.ToString() + ":";
+                }
+                else
+                {
+                    h = inth.ToString() + ":";
+                }
+                if (intm < 10)
+                {
+                    m = "0" + intm.ToString() + ":";
+                }
+                else
+                {
+                    m = intm.ToString() + ":";
+                }
+                if (ints < 10)
+                {
+                    s = "0" + ints.ToString();
+                }
+                else
+                {
+                    s = ints.ToString();
+                }
+                textBox2.Text = h + m + s;
+            }
+            else
+            {
+                ints = Convert.ToInt32(numericUpDown3.Value);
+                if (inth < 10)
+                {
+                    h = "0" + inth.ToString() + ":";
+                }
+                else
+                {
+                    h = inth.ToString() + ":";
+                }
+                if (intm < 10)
+                {
+                    m = "0" + intm.ToString() + ":";
+                }
+                else
+                {
+                    m = intm.ToString() + ":";
+                }
+                if (ints < 10)
+                {
+                    s = "0" + ints.ToString();
+                }
+                else
+                {
+                    s = ints.ToString();
+                }
+                textBox2.Text = h + m + s;
+            }
+
+        }
+        /// <summary>
+        /// 功能为获取系统时间组件timer1的实现
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            DateTime gettime = Convert.ToDateTime(DateTime.Now.ToString());
+            textBox1.Text = Convert.ToString(gettime);//将系统时间显示在textbox1上
+        }
+        /// <summary>
+        /// 取消闹钟按钮的实现
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer2.Enabled = false;//终止功能为获取时间差的组件启动
+            label2.Text = "闹钟已取消！";
+            label1.Text = "";
+        }
+
+    }
+}
