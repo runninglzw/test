210eee9af943eb2c3c549e75ee1d5a574ffede40
runninglzw <459437440@qq.com>
2015-05-02 19:59:19 +0800

    Socket编程
    使用C#实现一个简单的客户端和服务端之间的通信

---
+++ socket通信/客户端代码（socket）/ConsoleApplication2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] b = new byte[1024];
+            byte[] b2 = new byte[1024];
+            string send = "hello![final]";
+            b = Encoding.UTF8.GetBytes(send);
+            Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            //获得主机信息
+            IPHostEntry iphost = Dns.Resolve("lzw");
+            //使用主机的ip地址和指定端口号建立一个网络端点
+            IPEndPoint ip = new IPEndPoint(iphost.AddressList[0], 2112);
+            //使用这个网络端点连接远程服务器
+            s.Connect(ip);
+            Console.WriteLine("成功连接到服务器！");
+            //向服务器发送数据
+            s.Send(b);
+            int x = s.Receive(b2);
+            Console.WriteLine("从服务器返回的数据："+Encoding.UTF8.GetString(b2));
+            s.Shutdown(SocketShutdown.Both);
+            s.Close();
+        }
+    }
+}
---
+++ socket通信/服务端代码（socket）/ConsoleApplication1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("服务器已经开启！");
+            Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Stream,ProtocolType.Tcp);
+            //绑定一个IP地址和端口号
+            s.Bind(new IPEndPoint(IPAddress.Any,2112));
+            //开始监听
+            s.Listen(10);
+                string result = string.Empty;
+            //获得到连接到这台服务器的socket对象
+                Socket socket = s.Accept();
+            //读取socket对象上的数据
+                while (true)
+                {
+                    byte[] b1 = new byte[1024];
+                    int x = socket.Receive(b1);
+                    Console.WriteLine("正在接收！");
+                    result += Encoding.UTF8.GetString(b1, 0, x);
+                    if (result.IndexOf("[final]") > -1)
+                    {
+                        break;
+                    }
+                    //x = socket.Receive(b1);
+                }
+                Console.WriteLine("接收到的数据："+result);
+            //返回给客户端一个信息
+                string fanhui = "我已经接收到您的数据！";
+                byte[] b2 = Encoding.UTF8.GetBytes(fanhui);
+                socket.Send(b2);
+            //关闭socket对象的发送和接收功能
+                socket.Shutdown(SocketShutdown.Both);
+                socket.Close();
+        }
+    }
+}
