1887bfeede3cec25b379ce2b27d93cc262db398d
runninglzw <459437440@qq.com>
2015-06-24 21:14:05 +0800

    自定义浏览器

---
+++ 浏览器的制作/浏览器的制作/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace 浏览器的制作
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem导航 = new System.Windows.Forms.ToolStripMenuItem();
+            this.后退ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.前进ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.刷新ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.主页ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.查询ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem属性 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem打印浏览 = new System.Windows.Forms.ToolStripMenuItem();
+            this.停止ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.页面设置ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.打印ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripTextBox1 = new System.Windows.Forms.ToolStripTextBox();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.menuStrip1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem导航,
+            this.后退ToolStripMenuItem,
+            this.前进ToolStripMenuItem,
+            this.刷新ToolStripMenuItem,
+            this.主页ToolStripMenuItem,
+            this.查询ToolStripMenuItem,
+            this.toolStripMenuItem属性,
+            this.toolStripMenuItem1,
+            this.toolStripMenuItem打印浏览,
+            this.停止ToolStripMenuItem,
+            this.页面设置ToolStripMenuItem,
+            this.打印ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(586, 25);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem导航
+            // 
+            this.toolStripMenuItem导航.Name = "toolStripMenuItem导航";
+            this.toolStripMenuItem导航.Size = new System.Drawing.Size(44, 21);
+            this.toolStripMenuItem导航.Text = "导航";
+            this.toolStripMenuItem导航.Click += new System.EventHandler(this.toolStripMenuItem导航_Click);
+            // 
+            // 后退ToolStripMenuItem
+            // 
+            this.后退ToolStripMenuItem.Name = "后退ToolStripMenuItem";
+            this.后退ToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.后退ToolStripMenuItem.Text = "后退";
+            this.后退ToolStripMenuItem.Click += new System.EventHandler(this.后退ToolStripMenuItem_Click);
+            // 
+            // 前进ToolStripMenuItem
+            // 
+            this.前进ToolStripMenuItem.Name = "前进ToolStripMenuItem";
+            this.前进ToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.前进ToolStripMenuItem.Text = "前进";
+            this.前进ToolStripMenuItem.Click += new System.EventHandler(this.前进ToolStripMenuItem_Click);
+            // 
+            // 刷新ToolStripMenuItem
+            // 
+            this.刷新ToolStripMenuItem.Name = "刷新ToolStripMenuItem";
+            this.刷新ToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.刷新ToolStripMenuItem.Text = "刷新";
+            this.刷新ToolStripMenuItem.Click += new System.EventHandler(this.刷新ToolStripMenuItem_Click);
+            // 
+            // 主页ToolStripMenuItem
+            // 
+            this.主页ToolStripMenuItem.Name = "主页ToolStripMenuItem";
+            this.主页ToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.主页ToolStripMenuItem.Text = "主页";
+            this.主页ToolStripMenuItem.Click += new System.EventHandler(this.主页ToolStripMenuItem_Click);
+            // 
+            // 查询ToolStripMenuItem
+            // 
+            this.查询ToolStripMenuItem.Name = "查询ToolStripMenuItem";
+            this.查询ToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.查询ToolStripMenuItem.Text = "查询";
+            this.查询ToolStripMenuItem.Click += new System.EventHandler(this.查询ToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem属性
+            // 
+            this.toolStripMenuItem属性.Name = "toolStripMenuItem属性";
+            this.toolStripMenuItem属性.Size = new System.Drawing.Size(44, 21);
+            this.toolStripMenuItem属性.Text = "属性";
+            this.toolStripMenuItem属性.Click += new System.EventHandler(this.toolStripMenuItem属性_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(44, 21);
+            this.toolStripMenuItem1.Text = "保存";
+            this.toolStripMenuItem1.Click += new System.EventHandler(this.toolStripMenuItem1_Click);
+            // 
+            // toolStripMenuItem打印浏览
+            // 
+            this.toolStripMenuItem打印浏览.Name = "toolStripMenuItem打印浏览";
+            this.toolStripMenuItem打印浏览.Size = new System.Drawing.Size(68, 21);
+            this.toolStripMenuItem打印浏览.Text = "打印浏览";
+            this.toolStripMenuItem打印浏览.Click += new System.EventHandler(this.toolStripMenuItem打印浏览_Click);
+            // 
+            // 停止ToolStripMenuItem
+            // 
+            this.停止ToolStripMenuItem.Name = "停止ToolStripMenuItem";
+            this.停止ToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.停止ToolStripMenuItem.Text = "停止";
+            this.停止ToolStripMenuItem.Click += new System.EventHandler(this.停止ToolStripMenuItem_Click);
+            // 
+            // 页面设置ToolStripMenuItem
+            // 
+            this.页面设置ToolStripMenuItem.Name = "页面设置ToolStripMenuItem";
+            this.页面设置ToolStripMenuItem.Size = new System.Drawing.Size(68, 21);
+            this.页面设置ToolStripMenuItem.Text = "页面设置";
+            this.页面设置ToolStripMenuItem.Click += new System.EventHandler(this.页面设置ToolStripMenuItem_Click);
+            // 
+            // 打印ToolStripMenuItem
+            // 
+            this.打印ToolStripMenuItem.Name = "打印ToolStripMenuItem";
+            this.打印ToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.打印ToolStripMenuItem.Text = "打印";
+            this.打印ToolStripMenuItem.Click += new System.EventHandler(this.打印ToolStripMenuItem_Click);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripTextBox1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 25);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(586, 25);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripTextBox1
+            // 
+            this.toolStripTextBox1.Name = "toolStripTextBox1";
+            this.toolStripTextBox1.Size = new System.Drawing.Size(300, 25);
+            this.toolStripTextBox1.Text = "请输入网址或关键字";
+            this.toolStripTextBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.toolStripTextBox1_KeyDown);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 50);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(586, 211);
+            this.webBrowser1.TabIndex = 2;
+            this.webBrowser1.Navigated += new System.Windows.Forms.WebBrowserNavigatedEventHandler(this.webBrowser1_Navigated);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(586, 261);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "Form1";
+            this.Text = "欢迎使用刘振威自制浏览器";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem导航;
+        private System.Windows.Forms.ToolStripMenuItem 后退ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 前进ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 刷新ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 主页ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 查询ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem打印浏览;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripTextBox toolStripTextBox1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem属性;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem 停止ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 页面设置ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 打印ToolStripMenuItem;
+
+    }
+}
+
---
+++ 浏览器的制作/浏览器的制作/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 浏览器的制作
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void toolStripMenuItem导航_Click(object sender, EventArgs e)
+        {
+            navigate(toolStripTextBox1.Text);//自定义navigate方法
+            //前进ToolStripMenuItem.Enabled = webBrowser1.CanGoForward;
+            //WebBrowserNavigatedEventHandler navigated;
+        }
+
+        private void 后退ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoBack();//向文档的上一页导航
+            //前进ToolStripMenuItem.Enabled = webBrowser1.CanGoForward;
+        }
+
+        private void 前进ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoForward();//向文档的下一页导航
+        }
+
+        private void 刷新ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Refresh();//重新加载当前网页
+        }
+
+        private void 主页ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoHome();//导航到主页
+        }
+
+        private void 查询ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoSearch();//导航到默认搜索页必应搜索
+        }
+
+        private void toolStripMenuItem属性_Click(object sender, EventArgs e)
+        {
+            webBrowser1.ShowPropertiesDialog();//打开ie的属性对话框
+        }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            webBrowser1.ShowSaveAsDialog();//打开ie的保存网页对话框
+        }
+
+        private void toolStripMenuItem打印浏览_Click(object sender, EventArgs e)
+        {
+            webBrowser1.ShowPrintPreviewDialog();//打开打印预览对话框
+        }
+
+        private void 停止ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Stop();//取消所有挂起的导航并停止所有动态元素
+        }
+
+        private void 页面设置ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            webBrowser1.ShowPageSetupDialog();//打开页面设置对话框
+        }
+
+        private void 打印ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Print();//打印当前网页
+        }
+
+        private void webBrowser1_Navigated(object sender, WebBrowserNavigatedEventArgs e)
+        {
+            toolStripTextBox1.Text = webBrowser1.Url.ToString();//显示网址到文本框1中
+        }
+
+        private void toolStripTextBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                navigate(toolStripTextBox1.Text);//文本框1中按回车开始导航网页
+            }
+        }
+       /// <summary>
+       /// 判断url是否合法，合法则开始导航
+       /// </summary>
+        /// <param name="p"></param>toolStripTextBox1文本框中的url地址
+        private void navigate(string p)
+        {
+            if (String.IsNullOrEmpty(p)) return;
+            if (p.Equals("about:blank")) return;
+            if (!p.StartsWith("http://"))
+            {
+                p = "http://" + p;
+            }
+            try
+            {
+                webBrowser1.Navigate(new Uri(p),false);
+            }
+            catch (System.UriFormatException)
+            {
+                return;
+            }
+            //throw new NotImplementedException();
+        }
+        /*
+        private void webBrowser1_CanGoForwardChanged(object sender, EventArgs e)
+        {
+            前进ToolStripMenuItem.Enabled = webBrowser1.CanGoForward;
+        }*/
+    }
+}
