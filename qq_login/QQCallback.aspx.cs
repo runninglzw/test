678d2e0c28894922298bd134886320681c16171a
runninglzw <459437440@qq.com>
2015-07-17 21:44:01 +0800

    调用腾讯开放平台提供的.NET SDK进行登录

---
+++ qq_login/QQCallback.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using QConnectSDK;//请加入这个命名空间
+using QConnectSDK.Models;
+public partial class QQCallback : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (Request.Params["code"] != null)
+        {
+            QOpenClient qzone = null;
+            User currentUser = null;
+
+            var verifier = Request.Params["code"];
+            string state = Session["requeststate"].ToString();
+            qzone = new QOpenClient(verifier, state);
+            currentUser = qzone.GetCurrentUser();
+            if (null != currentUser)
+            {
+                this.result.Text = "成功登陆";
+                this.Nickname.Text = currentUser.Nickname;
+                this.Figureurl.ImageUrl = currentUser.Figureurl;
+            }
+            Session["QzoneOauth"] = qzone;
+        }
+    }
+}
